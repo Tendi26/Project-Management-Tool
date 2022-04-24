7581dfd19aece79b11e7e4cf9a7564b46f1d0ea5
tendi <codewithtendi@gmail.com>
2022-04-24 13:47:39 +0545

    field validation implemented

--- PMTool/src/main/java/com/ProjectManagementTool/PMTool/domain/Project.java
+++ PMTool/src/main/java/com/ProjectManagementTool/PMTool/domain/Project.java
@@ -1,6 +1,10 @@
 package com.ProjectManagementTool.PMTool.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
@@ -8,12 +12,20 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank(message = "Project name cannot be blank")
     private String projectName;
+    @NotBlank(message = "Project identifier cannot be blank")
+    @Size(min = 4,max = 5,message = "Project identifier can only be of 4 to 5 characters")
     private String projectIdentifier;
+    @NotBlank(message = "Project description is required")
     private String description;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date start_date;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date end_date;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date created_At;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date updated_At;
 
     public Project() {
